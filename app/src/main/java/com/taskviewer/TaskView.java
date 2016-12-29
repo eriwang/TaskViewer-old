d186e7c5161bd05bd6710b2a0579487f20059e3a
arcdawn <supersmash233@gmail.com>
2016-12-29 17:05:44 -0500

    Action Bar + Buttons
    Buttons created and functional. No methods attached to buttons yet
    except for a test button that displays text.

--- app/src/main/java/com/taskviewer/TaskView.java
+++ app/src/main/java/com/taskviewer/TaskView.java
@@ -3,6 +3,9 @@ package com.taskviewer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+
 public class TaskView extends AppCompatActivity {
 
 	@Override
@@ -11,5 +14,13 @@ public class TaskView extends AppCompatActivity {
 		setContentView(R.layout.activity_task_view);
 		Toolbar myToolbar = (Toolbar)findViewById(R.id.my_toolbar);
 		setSupportActionBar(myToolbar);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		MenuInflater inflater = getMenuInflater();
+		inflater.inflate(R.menu.toolbarmenu, menu);
+
+		return super.onCreateOptionsMenu(menu);
 	}
 }

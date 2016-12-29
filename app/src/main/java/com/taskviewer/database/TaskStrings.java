5aca738a13f983fa975edd8d4d84afc5cfd306e1
Eric Wang <johnericcw@gmail.com>
2016-12-29 18:14:03 -0500

    Restructure task slightly, create helpers for inserting tasks and querying tasks

---
+++ app/src/main/java/com/taskviewer/database/TaskStrings.java
@@ -0,0 +1,12 @@
+package com.taskviewer.database;
+
+import android.provider.BaseColumns;
+
+public class TaskStrings implements BaseColumns {
+	public static final String TABLE_NAME = "tasks";
+	public static final String COL_NAME_TITLE = "title";
+	public static final String COL_NAME_TIME = "time";
+	public static final String COL_NAME_DESC = "description";
+	public static final String COL_NAME_PRIORITY = "priority";
+	public static final String COL_NAME_COLOR = "color";
+}
--- app/src/main/java/com/taskviewer/database/TaskTableHelper.java
+++ app/src/main/java/com/taskviewer/database/TaskTableHelper.java
@@ -1,10 +1,13 @@
 package com.taskviewer.database;
 
-import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
-import android.provider.BaseColumns;
+import com.taskviewer.task.Task;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class TaskTableHelper extends SQLiteOpenHelper {
 	public static final int DATABASE_VERSION = 1;
@@ -32,24 +35,41 @@ public class TaskTableHelper extends SQLiteOpenHelper {
 	}
 
 	@Override
-	public void onOpen(SQLiteDatabase db) {
-	}
+	public void onOpen(SQLiteDatabase db) {}
 
 	@Override
-	public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-	}
+	public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {}
 
 	public void insertNewTask(Task task) {
 		SQLiteDatabase db = getWritableDatabase();
 		db.insert(TaskStrings.TABLE_NAME, null, task.getTaskContentValues());
 	}
 
-	public static class TaskStrings implements BaseColumns {
-		static final String TABLE_NAME = "tasks";
-		static final String COL_NAME_TITLE = "title";
-		static final String COL_NAME_TIME = "time";
-		static final String COL_NAME_DESC = "description";
-		static final String COL_NAME_PRIORITY = "priority";
-		static final String COL_NAME_COLOR = "color";
+	// TODO: only get some tasks at a time (i.e. WHERE clause)
+	public ArrayList<Task> getAllTasks() {
+		SQLiteDatabase db = getReadableDatabase();
+		Cursor cursor = db.query(TaskStrings.TABLE_NAME, null, null, null, null, null, null);
+
+		ArrayList<Task> taskList = new ArrayList<>();
+		while (cursor.moveToNext()) {
+			taskList.add(getNextTaskFromCursor(cursor));
+		}
+		cursor.close();
+
+		return taskList;
+	}
+
+	private Task getNextTaskFromCursor(Cursor cursor) {
+		String title = cursor.getString(
+				cursor.getColumnIndexOrThrow(TaskStrings.COL_NAME_TITLE));
+		int time = cursor.getInt(
+				cursor.getColumnIndexOrThrow(TaskStrings.COL_NAME_TIME));
+		String desc = cursor.getString(
+				cursor.getColumnIndexOrThrow(TaskStrings.COL_NAME_DESC));
+		int priority = cursor.getInt(
+				cursor.getColumnIndexOrThrow(TaskStrings.COL_NAME_PRIORITY));
+		int color = cursor.getInt(
+				cursor.getColumnIndexOrThrow(TaskStrings.COL_NAME_COLOR));
+		return new Task(title, time, desc, priority, color);
 	}
 }

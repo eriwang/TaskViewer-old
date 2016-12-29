a820043f483dc164609d3ee3905407ae631f1acd
arcdawn <supersmash233@gmail.com>
2016-12-29 15:15:30 -0500

    The App Bar
    No buttons yet but app bar is in the code.
    
    Working on menus

--- app/src/main/java/com/taskviewer/TaskView.java
+++ app/src/main/java/com/taskviewer/TaskView.java
@@ -2,12 +2,14 @@ package com.taskviewer;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
+import android.support.v7.widget.Toolbar;
 public class TaskView extends AppCompatActivity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_task_view);
+		Toolbar myToolbar = (Toolbar)findViewById(R.id.my_toolbar);
+		setSupportActionBar(myToolbar);
 	}
 }
